5a9da5b4992c7f838575321533f4050f7895054d
Yacob Madiana <yacob.madiana@gmail.com>
2019-02-21 14:45:28 +0700

    Complete upload testing and load text content to text area

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Amplify, { Auth } from "aws-amplify";
 import aws_exports from "./aws-exports";
 import { withAuthenticator } from "aws-amplify-react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Authenticator } from "aws-amplify-react";
 
 // View Components
 import Userman from "./views/users/Userman";
@@ -12,7 +13,22 @@ import Uploads from "./views/uploads/Uploads";
 import Sensors from "./views/sensors/Sensors";
 
 // Amplify Configuration
-Amplify.configure(aws_exports);
+//Amplify.configure(aws_exports);
+Amplify.configure({
+  Auth: {
+    identityPoolId: "us-west-2:f0eced49-7c0e-40cc-bc6a-2d4b623f044d", //REQUIRED - Amazon Cognito Identity Pool ID
+    region: "us-west-2", // REQUIRED - Amazon Cognito Region
+    userPoolId: "us-west-2_keom9fuep", //OPTIONAL - Amazon Cognito User Pool ID
+    userPoolWebClientId: "6kgv5jjo9lcb2d4q8r7g4jag35" //OPTIONAL - Amazon Cognito Web Client ID
+  } /*,
+  Storage: {
+      AWSS3: {
+          bucket: '', //REQUIRED -  Amazon S3 bucket
+          region: 'XX-XXXX-X', //OPTIONAL -  Amazon service region
+      }
+  }*/
+});
+
 const federated = {
   google_client_id:
     "595089086135-uobb25oa8ra3j92nlqbm79do9ieaaekc.apps.googleusercontent.com"
@@ -84,71 +100,73 @@ class App extends Component {
     const { activeItem } = this.state;
 
     return (
-      <Router>
-        <Grid>
-          <Grid.Column width={2}>
-            <Menu fluid vertical tabular color="blue">
-              <Link to="/">
-                <Menu.Item
-                  name="user"
-                  active={activeItem === "user"}
-                  onClick={this.handleItemClick}
-                />
-              </Link>
-              <Link to="/uploads">
-                <Menu.Item
-                  name="uploads"
-                  active={activeItem === "uploads"}
-                  onClick={this.handleItemClick}
-                />
-              </Link>
-              <Link to="/sensors">
-                <Menu.Item
-                  name="sensors"
-                  active={activeItem === "sensors"}
-                  onClick={this.handleItemClick}
-                />
-              </Link>
-            </Menu>
-            {routes.map((route, index) => (
-              // You can render a <Route> in as many places
-              // as you want in your app. It will render along
-              // with any other <Route>s that also match the URL.
-              // So, a sidebar or breadcrumbs or anything else
-              // that requires you to render multiple things
-              // in multiple places at the same URL is nothing
-              // more than multiple <Route>s.
-              <Route
-                key={index}
-                path={route.path}
-                exact={route.exact}
-                component={route.sidebar}
-              />
-            ))}
-          </Grid.Column>
-
-          <Grid.Column
-            textAlign="left"
-            stretched
-            padded
-            floated="left"
-            width={14}
-          >
-            <Segment textAlign="left" compact color="blue">
+      <div>
+        <Router>
+          <Grid>
+            <Grid.Column width={2}>
+              <Menu fluid vertical tabular color="blue">
+                <Link to="/">
+                  <Menu.Item
+                    name="user"
+                    active={activeItem === "user"}
+                    onClick={this.handleItemClick}
+                  />
+                </Link>
+                <Link to="/uploads">
+                  <Menu.Item
+                    name="uploads"
+                    active={activeItem === "uploads"}
+                    onClick={this.handleItemClick}
+                  />
+                </Link>
+                <Link to="/sensors">
+                  <Menu.Item
+                    name="sensors"
+                    active={activeItem === "sensors"}
+                    onClick={this.handleItemClick}
+                  />
+                </Link>
+              </Menu>
               {routes.map((route, index) => (
-                // Render more <Route>s with the same paths as
-                // above, but different components this time.
+                // You can render a <Route> in as many places
+                // as you want in your app. It will render along
+                // with any other <Route>s that also match the URL.
+                // So, a sidebar or breadcrumbs or anything else
+                // that requires you to render multiple things
+                // in multiple places at the same URL is nothing
+                // more than multiple <Route>s.
                 <Route
                   key={index}
                   path={route.path}
                   exact={route.exact}
-                  component={route.main}
+                  component={route.sidebar}
                 />
               ))}
-            </Segment>
-          </Grid.Column>
-        </Grid>
-      </Router>
+            </Grid.Column>
+
+            <Grid.Column
+              textAlign="left"
+              stretched
+              padded
+              floated="left"
+              width={14}
+            >
+              <Segment textAlign="left" compact color="blue">
+                {routes.map((route, index) => (
+                  // Render more <Route>s with the same paths as
+                  // above, but different components this time.
+                  <Route
+                    key={index}
+                    path={route.path}
+                    exact={route.exact}
+                    component={route.main}
+                  />
+                ))}
+              </Segment>
+            </Grid.Column>
+          </Grid>
+        </Router>
+      </div>
     );
   }
 }
--- src/views/uploads/Uploads.js
+++ src/views/uploads/Uploads.js
@@ -1,8 +1,31 @@
 import React from "react";
-import { Button, Checkbox, Icon, Table, Header } from 'semantic-ui-react';
-import Amplify, { Storage } from 'aws-amplify';
-import awsmobile from '../../aws-exports';
-Amplify.configure(awsmobile);
+import {
+  Button,
+  Checkbox,
+  Icon,
+  Table,
+  Header,
+  TextArea,
+  Form
+} from "semantic-ui-react";
+import Amplify, { Storage } from "aws-amplify";
+import { S3Text } from "aws-amplify-react";
+import awsmobile from "../../aws-exports";
+//Amplify.configure(awsmobile);
+Amplify.configure({
+  Auth: {
+    identityPoolId: "us-west-2:f0eced49-7c0e-40cc-bc6a-2d4b623f044d", //REQUIRED - Amazon Cognito Identity Pool ID
+    region: "us-west-2", // REQUIRED - Amazon Cognito Region
+    userPoolId: "us-west-2_keom9fuep", //OPTIONAL - Amazon Cognito User Pool ID
+    userPoolWebClientId: "6kgv5jjo9lcb2d4q8r7g4jag35" //OPTIONAL - Amazon Cognito Web Client ID
+  },
+  Storage: {
+    AWSS3: {
+      bucket: "mapiduserstorage", //REQUIRED -  Amazon S3 bucket
+      region: "us-west-2" //OPTIONAL -  Amazon service region
+    }
+  }
+});
 
 class Uploads extends React.Component {
   constructor(props) {
@@ -10,117 +33,168 @@ class Uploads extends React.Component {
     // Declare States
     this.state = {
       name: "",
-      filename: ""
+      filename: "",
+      file: null,
+      textcontent:""
     };
     // Declare Methods
     this.onUpload = this.onUpload.bind(this);
     this.uploadStart = this.uploadStart.bind(this);
+    this.getFile = this.getFile.bind(this);
   }
-  componentDidMount() {
-    
-  }
+  componentDidMount() {}
   onUpload(event) {
-    console.log('Hey : ',event);
+    console.log("Hey : ", event);
     this.setState({
-      filename: event.name
+      filename: event.name,
+      file: event
     });
   }
-  uploadStart(){
-    console.log('Starting Upload Progress');
+  uploadStart() {
+    console.log("Starting Upload Progress");
+    // Upload File
+    // Storage.put(this.state.filename, this.state.file)
+    //   .then(result => console.log(result))
+    //   .catch(err => console.log(err));
+    // Upload File to a private bucket
+    Storage.put(this.state.filename, this.state.file, {
+      progressCallback(progress) {
+        console.log(`Uploaded: ${progress.loaded}/${progress.total}`);
+      },
+      level: "private",
+      contentType: "text/plain"
+    })
+      .then(result => console.log(result))
+      .catch(err => console.log(err));
+  }
+  getFile() {
+    Storage.get("MyUltimate.txt", { level: "private" })
+      .then(result => {
+        console.log(result);
+        fetch(result)
+          .then(r => r.text())
+          .then(text => {
+            //console.log(text);
+            this.setState({
+              textcontent: text
+            });
+          });
+      })
+      .catch(err => console.log(err));
   }
   render() {
     return (
       <div>
-      <Header as="h2">
+        <Header as="h2">
           <Icon name="cloud upload" />
           <Header.Content>
             Cloud Upload
-            <Header.Subheader>Upload your geojson files to mapid cloud</Header.Subheader>
+            <Header.Subheader>
+              Upload your geojson files to mapid cloud
+            </Header.Subheader>
           </Header.Content>
         </Header>
-      <Table compact celled definition>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell />
-          <Table.HeaderCell>File Name</Table.HeaderCell>
-          <Table.HeaderCell>Geojson Type</Table.HeaderCell>
-          <Table.HeaderCell>View Type</Table.HeaderCell>
-          <Table.HeaderCell>Icon</Table.HeaderCell>
-          <Table.HeaderCell>Edit</Table.HeaderCell>
-          <Table.HeaderCell>Delete</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-  
-      <Table.Body>
-        <Table.Row>
-          <Table.Cell collapsing>
-            <Checkbox slider />
-          </Table.Cell>
-          <Table.Cell>Bandung.geojson</Table.Cell>
-          <Table.Cell>Fill</Table.Cell>
-          <Table.Cell>Standard</Table.Cell>
-          <Table.Cell>museum-15</Table.Cell>
-          <Table.Cell>Edit</Table.Cell>
-          <Table.Cell>Delete</Table.Cell>
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell collapsing>
-            <Checkbox slider />
-          </Table.Cell>
-          <Table.Cell>Jakarta.geojson</Table.Cell>
-          <Table.Cell>Line</Table.Cell>
-          <Table.Cell>Standard</Table.Cell>
-          <Table.Cell>museum-15</Table.Cell>
-          <Table.Cell>Edit</Table.Cell>
-          <Table.Cell>Delete</Table.Cell>
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell collapsing>
-            <Checkbox slider />
-          </Table.Cell>
-          <Table.Cell>Semarang.geojson</Table.Cell>
-          <Table.Cell>Point</Table.Cell>
-          <Table.Cell>Standard</Table.Cell>
-          <Table.Cell>museum-15</Table.Cell>
-          <Table.Cell>Edit</Table.Cell>
-          <Table.Cell>Delete</Table.Cell>
-        </Table.Row>
-      </Table.Body>
-  
-      <Table.Footer fullWidth>
-        <Table.Row>
-          <Table.HeaderCell />
-          <Table.HeaderCell colSpan='4'>
-            <Button floated='right' icon labelPosition='left' primary size='small'>
-              <Icon name='user' /> Add Empty Layer
-            </Button>
-            <UploadButton label='Choose File' onUpload={this.onUpload}></UploadButton>
-            <Button size='small' onClick={this.uploadStart}>Upload</Button>
-            <span>{this.state.filename}</span>
-          </Table.HeaderCell>
-          <Table.HeaderCell />
-          <Table.HeaderCell />
-        </Table.Row>
-      </Table.Footer>
-    </Table>
+        <Table compact celled definition>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell />
+              <Table.HeaderCell>File Name</Table.HeaderCell>
+              <Table.HeaderCell>Geojson Type</Table.HeaderCell>
+              <Table.HeaderCell>View Type</Table.HeaderCell>
+              <Table.HeaderCell>Icon</Table.HeaderCell>
+              <Table.HeaderCell>Edit</Table.HeaderCell>
+              <Table.HeaderCell>Delete</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell collapsing>
+                <Checkbox slider />
+              </Table.Cell>
+              <Table.Cell>Bandung.geojson</Table.Cell>
+              <Table.Cell>Fill</Table.Cell>
+              <Table.Cell>Standard</Table.Cell>
+              <Table.Cell>museum-15</Table.Cell>
+              <Table.Cell>Edit</Table.Cell>
+              <Table.Cell>Delete</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell collapsing>
+                <Checkbox slider />
+              </Table.Cell>
+              <Table.Cell>Jakarta.geojson</Table.Cell>
+              <Table.Cell>Line</Table.Cell>
+              <Table.Cell>Standard</Table.Cell>
+              <Table.Cell>museum-15</Table.Cell>
+              <Table.Cell>Edit</Table.Cell>
+              <Table.Cell>Delete</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell collapsing>
+                <Checkbox slider />
+              </Table.Cell>
+              <Table.Cell>Semarang.geojson</Table.Cell>
+              <Table.Cell>Point</Table.Cell>
+              <Table.Cell>Standard</Table.Cell>
+              <Table.Cell>museum-15</Table.Cell>
+              <Table.Cell>Edit</Table.Cell>
+              <Table.Cell>Delete</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+
+          <Table.Footer fullWidth>
+            <Table.Row>
+              <Table.HeaderCell />
+              <Table.HeaderCell colSpan="4">
+                <Button
+                  floated="right"
+                  icon
+                  labelPosition="left"
+                  primary
+                  size="small"
+                  onClick={this.getFile}
+                >
+                  <Icon name="user" /> Add Empty Layer
+                </Button>
+                <UploadButton label="Choose File" onUpload={this.onUpload} />
+                <Button size="small" onClick={this.uploadStart}>
+                  Upload
+                </Button>
+                <span>{this.state.filename}</span>
+              </Table.HeaderCell>
+              <Table.HeaderCell />
+              <Table.HeaderCell />
+            </Table.Row>
+          </Table.Footer>
+        </Table>
+        <Form>
+          <TextArea value={this.state.textcontent} placeholder="Click to display JSON content" autoHeight />
+        </Form>
       </div>
     );
   }
 }
 
-function UploadButton({label, onUpload, id}) {
+function UploadButton({ label, onUpload, id }) {
   let fileInput = null;
   // If no id was specified, generate a random one
-  const uid = id || Math.random().toString(36).substring(7);
+  const uid =
+    id ||
+    Math.random()
+      .toString(36)
+      .substring(7);
 
   return (
     <span>
       <label htmlFor={uid} className="ui icon button">
-        <i className="upload icon"></i>
+        <i className="upload icon" />
         {label}
       </label>
-      <input type="file" id={uid}
-        style={{display: "none"}}
+      <input
+        type="file"
+        id={uid}
+        style={{ display: "none" }}
         onChange={() => {
           onUpload(fileInput.files[0]);
         }}
